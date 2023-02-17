f1dfeefb9f0f0ae81333689513bf574a2de60d04
Nathan Spencer <natekspencer@gmail.com>
2023-02-17 10:40:25 -0700

    Use async_forward_entry_setups instead of async_setup_platforms

--- custom_components/smartweatherudp/__init__.py
+++ custom_components/smartweatherudp/__init__.py
@@ -34,7 +34,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         host=entry.data.get(CONF_HOST, DEFAULT_HOST)
     )
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     @callback
     def device_discovered(device: WeatherFlowDevice) -> None:
